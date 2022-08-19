dc06de569113b9932047931a16ea549346929b14
Ulisses Melo <ulisses.mbfm@gmail.com>
2022-08-19 16:51:33 -0300

    criando um dataframe

---
+++ criando_dataframe.R
@@ -0,0 +1,6 @@
+clubes<- c("Sport", "Santa Cruz", "Náutico")
+estadios<- c("Ilha do Retiro", "Arruda", "Aflitos")
+capacidade<- c(35000, 50000, 18000)
+
+futebol_pernambuco <- data.frame(clubes, estadios, capacidade)
+str(futebol_pernambuco)
