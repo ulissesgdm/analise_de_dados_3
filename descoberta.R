96989ab5dafbb8355a0a6cd5dd04819bd226247a
Ulisses Melo <ulisses.mbfm@gmail.com>
2022-09-07 17:27:33 -0300

    descoberta

---
+++ descoberta.R
@@ -0,0 +1,7 @@
+pacman::p_load(funModeling, tidyverse) 
+
+glimpse(colon) # olhada nos dados
+status(colon) # estrutura dos dados (missing etc)
+freq(colon) # frequência das variáveis fator
+plot_num(colon) # exploração das variáveis numéricas
+profiling_num(colon) # estatísticas das variáveis numéricas
